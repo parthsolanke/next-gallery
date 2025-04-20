f5e9d5dcff01c3f2f31ad332ffbca2da4366c1f5
parthsolanke <parthsolanke2002@gmail.com>
2025-04-20 18:21:47 +0530

    feat: add auth for home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,30 @@ import { index } from "drizzle-orm/gel-core";
 import Link from "next/link";
 import Image from "next/image";
 import { db } from "@/server/db"
+import { SignedOut, SignedIn, SignInButton } from "@clerk/nextjs"
+
+async function Images() {
+	const images = await db.query.images.findMany({
+		orderBy: (model, { desc }) => desc(model.id),
+	});
+
+	return (
+		<div className="flex flex-wrap gap-4">
+		{[ ...images, ...images, ...images, ...images].map((image, index) => (
+				<div key={image.id + '-' + index} className="flex flex-col w-48">
+					<Image
+						src={image.url}
+						alt="image"
+						width={200}
+						height={200}
+						loading="lazy"
+					/>
+					<div>{image.name}</div>
+				</div>
+			))}
+		</div>
+	)
+}
 
 export default async function HomePage() {
 	const images = await db.query.images.findMany({
@@ -12,20 +36,13 @@ export default async function HomePage() {
 
 	return (
 		<main>
-			<div className="flex flex-wrap gap-4">
-				{[ ...images, ...images, ...images, ...images].map((image, index) => (
-						<div key={image.id + '-' + index} className="flex flex-col w-48">
-							<Image
-								src={image.url}
-								alt="image"
-								width={200}
-								height={200}
-								loading="lazy"
-							/>
-							<div>{image.name}</div>
-						</div>
-					))}
-			</div>
+			<SignedOut>
+				<div className="text-2xl w-full h-full text-center">Please sign in above to view the gallery.</div>
+			</SignedOut>
+
+			<SignedIn>
+				<Images />
+			</SignedIn>
 		</main>
 	);
 }
