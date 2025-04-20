e21eeed1ebfc20981574944a7e615d3cd1445a97
parthsolanke <parthsolanke2002@gmail.com>
2025-04-20 11:17:09 +0530

    fix: add force dynamics to avoid Next.js prerendering issue

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
+export const dynamic = "force-dynamic";
+
 import { index } from "drizzle-orm/gel-core";
 import Link from "next/link";
 import Image from "next/image";
 import { db } from "@/server/db"
-
-// export const dynamic = "force-dynamic";
 
 export default async function HomePage() {
 	const images = await db.query.images.findMany({
